8621d56b878edd4df909f46fbbc142e2b5ee1e2a
Hemadri <142394039+hema7392@users.noreply.github.com>
2026-08-09 21:11:38 +0530

    Update 0035_Search_Insert_Position.java

--- Arrays/0035_Search_Insert_Position.java
+++ Arrays/0035_Search_Insert_Position.java
@@ -1,12 +1,26 @@
 class Solution {
-    public int removeElement(int[] nums, int val) {
-        int i=0;
-        for(int j=0;j<nums.length;j++){
-            if(nums[j]!=val){
-                nums[i]=nums[j];
-                i++;
+    public int searchInsert(int[] nums, int target) {
+
+        int left = 0;
+        int right = nums.length - 1;
+
+        while (left <= right) {
+
+            int mid = left + (right - left) / 2;
+
+            if (nums[mid] == target) {
+                return mid;
+            }
+
+            else if (nums[mid] < target) {
+                left = mid + 1;
+            }
+
+            else {
+                right = mid - 1;
             }
         }
-        return i;
+
+        return left;
     }
 }
