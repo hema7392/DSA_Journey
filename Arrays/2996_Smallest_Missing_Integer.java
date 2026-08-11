30dda56582fe54de2823901623a65de8357e1241
Hemadri <142394039+hema7392@users.noreply.github.com>
2026-08-11 22:58:38 +0530

    Create 2996_Smallest_Missing_Integer.java

---
+++ Arrays/2996_Smallest_Missing_Integer.java
@@ -0,0 +1,26 @@
+class Solution {
+    public int missingInteger(int[] nums) {
+        int sum=nums[0];
+        for(int i=1;i<nums.length;i++){
+            if(nums[i] == nums[i-1] + 1){
+                sum+=nums[i];
+            }
+            else{
+                break;
+            }
+        }
+        int answer=sum;
+        while(contains(nums,answer)){
+            answer++;
+        }
+        return answer;
+    }
+    public boolean contains(int[] nums,int target){
+        for(int num:nums){
+            if(num==target){
+                return true;
+            }
+        }
+        return false;
+    }
+}
