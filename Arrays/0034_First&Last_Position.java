235ad99ca834f74fef8bc28e07247185ffe3a00e
Hemadri <142394039+hema7392@users.noreply.github.com>
2026-08-17 23:03:27 +0530

    Create 0034_First&Last_Position.java

---
+++ Arrays/0034_First&Last_Position.java
@@ -0,0 +1,32 @@
+class Solution {
+    public int[] searchRange(int[] nums, int target) {
+        int first= search(nums,target,true);
+        int last= search(nums,target,false);
+        return new int[]{first,last};
+    }
+    public int search(int[] nums, int target,boolean first)
+    {
+        int left=0;
+        int right=nums.length-1;
+        int ans=-1;
+        while(left<=right){
+            int mid=(left+right)/2;
+            if(nums[mid]==target){
+                ans=mid;
+                if(first){
+                    right=mid-1;
+                }
+                else{
+                    left=mid+1;
+                }
+            }
+            else if(nums[mid]<target){
+                left=mid+1;
+            }
+            else{
+                right=mid-1;
+            }
+        }
+        return ans;
+    }
+}
